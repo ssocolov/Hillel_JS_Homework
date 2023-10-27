70a1045c2e376d24e1166cb6ae1a327a3afbcf78
ssocolov <favouriteseason2006@gmail.com>
2023-10-27 16:08:52 +0300

    DZ_9_ver.2

--- DZ_9.js
+++ DZ_9.js
@@ -1,15 +1,15 @@
 // Задача 1 "Тусувати масив"
-function myBlend(arr) {
-        for (let i = arr.length - 1; i > 0; i--) {   //перебираем элементы массива в обратном порядке
-        let randomIndex = Math.floor(Math.random() * (i + 1));    //генерируем случайный индекс в границах массива
-        let temp = arr[i];            //сохраняем значение поточного элемента массива arr[i] в переменной temp
-        arr[i] = arr[randomIndex];      // меняем поточный элемент на arr[randomIndex]
-        arr[randomIndex] = temp;        // меняем элемент arr[randomIndex] на начальное значение temp
-    }
-}
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-myBlend(arr);
-console.log(arr);
+// function myBlend(arr) {
+//         for (let i = arr.length - 1; i > 0; i--) {   //перебираем элементы массива в обратном порядке
+//         let randomIndex = Math.floor(Math.random() * (i + 1));    //генерируем случайный индекс в границах массива
+//         let temp = arr[i];            //сохраняем значение поточного элемента массива arr[i] в переменной temp
+//         arr[i] = arr[randomIndex];      // меняем поточный элемент на arr[randomIndex]
+//         arr[randomIndex] = temp;        // меняем элемент arr[randomIndex] на начальное значение temp
+//     }
+// }
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// myBlend(arr);
+// console.log(arr);
 
 // Задача 2 "BigBoss"
 const company = {
@@ -41,6 +41,12 @@ const company = {
                             type: 'subSubCompany',
                             uses: 'Рішення для продажу квитків',
                             sells: 'Рішення для продажу квитків',
+                            partners: [{
+                                name: 'Нова компанія',
+                                type: 'subSubCompany',
+                                uses: 'Якесь рішення',
+                                sells: 'Ще одне',
+                            }]
                         }
                     ]
                 }
@@ -50,33 +56,46 @@ const company = {
             name: 'Клієнт 2',
             type: 'subCompany',
             uses: 'ПО для продажу квитків',
-            sells: 'Рішення для продажу квитків'
+            sells: 'Рішення для продажу квитків',
+            partners: [{
+                name: "Qwerty",
+                type: "subSubCompany",
+                uses: "Рішення для продажу квитків",
+                sells: "Рішення для продажу квитків",
+                partners: [{
+                    name: 'Qwerty1',
+                    type: 'subSubCompany',
+                    uses: 'any',
+                    sells: 'any',
+                    partners: [{
+                        name: 'Qwerty2',
+                        type: 'subSubCompany',
+                        uses: 'tickets',
+                        sells: 'sale'
+                    }]
+                }]
+            }]
         }
     ]
 };
 
-function findValueByKey(companyName) {
-        for (let i in company.clients) {
-            if (company.clients[i].name === companyName) {
-                return Object.entries(company.clients[i]);
-            } else {
-                for (let o in company.clients[i].partners) {
-                    if (company.clients[i].partners[o].name === companyName) {
-                        return Object.entries(company.clients[i].partners[o]);
-                    } else {
-                        for (let p in company.clients[i].partners[o].partners) {
-                            if (company.clients[i].partners[o].partners[p].name === companyName) {
-                                return Object.entries(company.clients[i].partners[o].partners[p]);
-                                } else {
-                                    if (companyName === 'Велика Компанія') {
-                                        return Object.entries(company);
-                                }
-                            }
+function findValueByKey(object, companyName) {
+    if (object.name === companyName) {
+        return object;
+    } else {
+        for (let i in object) {
+            if (Array.isArray(object[i])) {
+                for (let o = 0; o < object[i].length; o++) {
+                    if (typeof object[i][o] === "object") {
+                        let rec_findValueByKey = findValueByKey(object[i][o], companyName);
+                        if (rec_findValueByKey != undefined) {
+                            return rec_findValueByKey;
                         }
                     }
                 }
             }
         }
+    }
+    return null;
 }
-
-console.log(findValueByKey('Клієнт 2'));
+console.log(findValueByKey(company, 'Qwerty'));
